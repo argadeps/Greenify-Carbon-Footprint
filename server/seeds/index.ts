3f67d1645ab4b996fb8e9a2f898f025d9c84c31d
Sherard Howe <mr.s.j.howe1@icloud.com>
2025-02-11 18:00:56 -0600

    feature: added seeding for users and emission factors

---
+++ server/seeds/index.ts
@@ -0,0 +1,23 @@
+import { seedEmissionFactors} from './emisssion-factors-seeds.js';
+import { seedUsers } from './user-seeds.js';
+import sequelize from '../config/connection.js';
+
+const seedAll = async (): Promise<void> => {
+  try {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+
+    await seedUsers();
+    console.log('\n----- USERS SEEDED -----\n');
+
+    await seedEmissionFactors();
+    console.log('\n----- EMISSIONFACTORS SEEDED -----\n');
+    
+    process.exit(0);
+  } catch (error) {
+    console.error('Error seeding database:', error);
+    process.exit(1);
+  }
+};
+
+seedAll();
